d44d34f3c067f45a7bbca767685c64823523e3fc
unlimish <mail@unlimish.xyz>
2020-10-13 18:16:37 +0900

    Make RandomWords as a module

--- list_view_app/flutter_app_listview/lib/main.dart
+++ list_view_app/flutter_app_listview/lib/main.dart
@@ -3,10 +3,22 @@ import 'package:english_words/english_words.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class RandomWords extends StatefulWidget {
+  @override
+  _RandomWordsState createState() => _RandomWordsState();
+}
+
+class _RandomWordsState extends State<RandomWords> {
   @override
   Widget build(BuildContext context) {
     final wordPair = WordPair.random();
+    return Text(wordPair.asPascalCase);
+  }
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Welcome to Flutter',
       home: Scaffold(
@@ -14,7 +26,7 @@ class MyApp extends StatelessWidget {
           title: const Text('Welcome to Flutter'),
         ),
         body: Center(
-          child: Text(wordPair.asPascalCase),
+          child: RandomWords(),
         ),
       ),
     );
