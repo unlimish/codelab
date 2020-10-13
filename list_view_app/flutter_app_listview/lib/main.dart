d5af58953cd2f404c39409371a8558345cb04fe1
unlimish <mail@unlimish.xyz>
2020-10-13 18:26:17 +0900

    Add listBuilder but these are NOT used yet on build()

--- list_view_app/flutter_app_listview/lib/main.dart
+++ list_view_app/flutter_app_listview/lib/main.dart
@@ -9,6 +9,34 @@ class RandomWords extends StatefulWidget {
 }
 
 class _RandomWordsState extends State<RandomWords> {
+  final List<WordPair> _suggetions = <WordPair>[];
+  final TextStyle _biggerFont = const TextStyle(fontSize: 18);
+
+  Widget _buildRow(WordPair pair) {
+    return ListTile(
+      title: Text(
+        pair.asPascalCase,
+        style: _biggerFont,
+      ),
+    );
+  }
+
+  Widget _buildSuggestions() {
+    return ListView.builder(
+      padding: const EdgeInsets.all(16),
+      itemBuilder: (BuildContext _context, int i) {
+        if (i.isOdd) {
+          return Divider();
+        }
+        final int index = i ~/ 2;
+        if (index >= _suggetions.length) {
+          _suggetions.addAll(generateWordPairs().take(10));
+        }
+        return _buildRow(_suggetions[index]);
+      }
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     final wordPair = WordPair.random();
