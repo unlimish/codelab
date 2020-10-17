739cae5bb3c7d5798941b0befc50b055b9920ce9
unlimish <mail@unlimish.xyz>
2020-10-17 15:22:30 +0900

    Change text by random when pressed roll button

--- android/DiceRoller/app/src/main/java/xyz/unlimish/diceroller/MainActivity.kt
+++ android/DiceRoller/app/src/main/java/xyz/unlimish/diceroller/MainActivity.kt
@@ -3,6 +3,7 @@ package xyz.unlimish.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
@@ -11,13 +12,19 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val rollButton: Button = findViewById(R.id.roll_button)
+
+
         rollButton.setOnClickListener {
             rollDice()
         }
     }
 
     private fun rollDice() {
-        Toast.makeText(this, "button clicked",
-        Toast.LENGTH_SHORT).show()
+        val randomInt = (1..6).random()
+//        Toast.makeText(this, "button clicked"
+//        Toast.LENGTH_SHORT).show()
+
+        val resultText: TextView = findViewById(R.id.result_text)
+        resultText.text = randomInt.toString()
     }
 }
