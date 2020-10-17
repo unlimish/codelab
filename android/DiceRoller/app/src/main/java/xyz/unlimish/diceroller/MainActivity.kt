878e8cf26c5f7cc1e04940e979d6005b8cc3163c
unlimish <mail@unlimish.xyz>
2020-10-17 13:55:45 +0900

    Init android/DiceRoller

---
+++ android/DiceRoller/app/src/main/java/xyz/unlimish/diceroller/MainActivity.kt
@@ -0,0 +1,11 @@
+package xyz.unlimish.diceroller
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
